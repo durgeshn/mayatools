41b5743fb651a569a93e730a5b1f1f77bdf50bd2
durgeshn <durgesh.n@philmcgi.com>
2017-04-18 11:03:54 +0530

    monCl

--- mayatools/batch/pcg_playblast/pass.py
+++ mayatools/batch/pcg_playblast/pass.py
@@ -1,7 +1,7 @@
 import os
 from ConfigParser import SafeConfigParser
 
-mayaFilePath = r"D:\temp\BDG105_004_layNew.ma"
+mayaFilePath = r"D:\temp\BDG105_004_lay.ma"
 configFilePath = os.path.dirname(__file__) + '/setting.config'
 
 
@@ -10,9 +10,43 @@ class getSceneDetails(object):
         self.mayaFilePath = mayaFilePath
         self.configFilePath = configFilePath
 
-    def get_cams(self):
-        all_cams = list()
+    @property
+    def getParser(self):
+        parser = SafeConfigParser()
+        parser.read(self.configFilePath)
+        return parser
+
+    def getReferences(self, nameFilter=['cam', 'Cam', 'CAM']):
+        allReferences = []
+        camReferences = []
         with open(self.mayaFilePath, 'r') as fi:
+            for line in fi.readlines():
+                if line.startswith('file -rdi'):
+                    allReferences.append(line.split(' ')[-1].replace('"', '').replace(';', '').strip())
+        for each in allReferences:
+            for filt in nameFilter:
+                if each.find(filt) != -1:
+                    camReferences.append(each)
+        return camReferences
+
+    def get_cams(self):
+        all_cams = []
+        mainFileCams = self._getCams(self.mayaFilePath)
+        for each in mainFileCams:
+            all_cams.append(each)
+        allReferences = self.getReferences()
+        for each in allReferences:
+            for eachFile in self._getCams(each):
+                all_cams.append(eachFile)
+        return list(set(all_cams))
+
+    def _getCams(self, mayaFilePath):
+        all_cams = list()
+        if mayaFilePath.startswith('$'):
+            env = mayaFilePath.split('/')[0]
+            newPath = self.getParser.get('env_path', env[1:])
+            mayaFilePath = mayaFilePath.replace(env, newPath)
+        with open(mayaFilePath, 'r') as fi:
             for line in fi.readlines():
                 if line.startswith('createNode camera'):
                     all_cams.append(line.split(' ')[-1].strip().replace(';', '').replace('"', ''))
@@ -28,17 +62,12 @@ class getSceneDetails(object):
                     endFrame = line.split('-min ')[-1].split(' -max ')[1].split(' -ast')[0]
         return startFrame, endFrame
 
-    @property
-    def getParser(self):
-        parser = SafeConfigParser()
-        parser.read(self.configFilePath)
-        return parser
-
     def getResolution(self):
         print self.getParser.options('resolution')
 
 
 a = getSceneDetails(mayaFilePath, configFilePath)
-print a.getFrameRange()
+# print a.getFrameRange()
+# print a.get_cams()
+# print a.getResolution()
 print a.get_cams()
-print a.getResolution()
---
+++ mayatools/utils/test2.py
@@ -0,0 +1,3 @@
+a = '$PROD_SERVER/01_SAISON_1/13_PRODUCTION/03_ASSETS/03_3D/ch/ch_rosie/rig/ch_rosie_rig.ma'
+
+print a.find('cam')
